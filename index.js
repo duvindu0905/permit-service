77ffea9ce336604d51092762669f58e00c9d50d3
duvindu0905 <duvindudushan@gmail.com>
2024-12-03 01:44:30 +0530

    api call to route service is created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app');  // Import the app from app.js
 const dotenv = require('dotenv');  // To load environment variables from .env file
 dotenv.config();  // Configure dotenv
 
-const PORT = process.env.PORT || 8080;  // Set port from environment variable or default to 8080
+const PORT = process.env.PORT || 8081;  // Set port from environment variable or default to 8080
 
 // Start the Express server
 app.listen(PORT, () => {
